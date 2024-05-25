f5e1557c0b612f9c77f467ca00b888addf4d8a09
Szymon <szymon.lapko@gmail.com>
2024-05-25 21:45:08 +0200

    changed dashboard and route table display

--- app/src/main/java/pl/lapko/vyosmanager/ui/displays/DashboardDisplay.kt
+++ app/src/main/java/pl/lapko/vyosmanager/ui/displays/DashboardDisplay.kt
@@ -1,17 +1,21 @@
 package pl.lapko.vyosmanager.ui.displays
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -24,7 +28,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,22 +40,49 @@ fun DashboardDisplay(onRequest : () -> Unit, onSuccess: () -> Unit, onError: (Ex
     var hostname by remember { mutableStateOf("") }
     var hostnameEditMode by remember { mutableStateOf(false) }
     var interfaces by remember { mutableStateOf("") }
+    var memory by remember { mutableStateOf("") }
+    var uptime by remember { mutableStateOf("") }
+    var cpu by remember { mutableStateOf("") }
     var displayDashboard by remember { mutableStateOf(false) }
     LaunchedEffect(Unit) {
         VyOSConnection.showVyOSData(
             "\"host\", \"name\"",
             onSuccess = {
                 hostname = it.data!!.textValue().removeSuffix("\n")
-                VyOSConnection.showVyOSData(
-                    "\"interfaces\"",
-                    onSuccess = {
-                        interfaces = it.data!!.textValue()
-                        interfaces = interfaces.replace("IP Address", "IP-Address")
-                        displayDashboard = true
-                    }, onError = {
-                        onError(it)
-                    }
-                )
+            }, onError = {
+                onError(it)
+            }
+        )
+        VyOSConnection.showVyOSData(
+            "\"interfaces\", \"summary\"",
+            onSuccess = {
+                interfaces = it.data!!.textValue().removeSuffix("\n")
+                interfaces = interfaces.replace("IP Address", "IP-Address")
+            }, onError = {
+                onError(it)
+            }
+        )
+        VyOSConnection.showVyOSData(
+            "\"system\", \"memory\"",
+            onSuccess = {
+                memory = it.data!!.textValue().removeSuffix("\n")
+            }, onError = {
+                onError(it)
+            }
+        )
+        VyOSConnection.showVyOSData(
+            "\"system\", \"uptime\"",
+            onSuccess = {
+                uptime = it.data!!.textValue().removeSuffix("\n")
+            }, onError = {
+                onError(it)
+            }
+        )
+        VyOSConnection.showVyOSData(
+            "\"system\", \"cpu\"",
+            onSuccess = {
+                cpu = it.data!!.textValue().removeSuffix("\n")
+                displayDashboard = true
             }, onError = {
                 onError(it)
             }
@@ -60,12 +90,13 @@ fun DashboardDisplay(onRequest : () -> Unit, onSuccess: () -> Unit, onError: (Ex
     }
     if (displayDashboard) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(20.dp))
             Text(
                 text = "DASHBOARD",
-                fontWeight = FontWeight.Bold
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
             )
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(20.dp))
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Text(text = "Hostname: ")
                 TextField(
@@ -101,39 +132,110 @@ fun DashboardDisplay(onRequest : () -> Unit, onSuccess: () -> Unit, onError: (Ex
                     }
                 )
             }
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(20.dp))
             Text(
                 text = "INTERFACE STATUS",
                 fontWeight = FontWeight.Bold
             )
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(20.dp))
             LazyColumn {
                 itemsIndexed(interfaces.split("\n")) { index, value ->
-                    if (value.matches(Regex("^Codes:.*"))) {
-                        Text(
-                            text = value,
-                            fontSize = 10.sp
-                        )
-                    } else if (!value.matches(Regex("^(-+\\s*)+$"))) {
-                        LazyRow {
-                            itemsIndexed(Regex("\\S*\\s*").findAll(value).map { it.value }
-                                .toList()) { index, value ->
-                                Text(
-                                    text = value,
-                                    fontFamily = FontFamily.Monospace,
-                                    modifier = Modifier.border(
-                                        width = 1.dp,
-                                        color = Color.White
+                    var content by remember { mutableStateOf(listOf<String>()) }
+                    var displayDetails by remember { mutableStateOf(false) }
+                    var addresses by remember { mutableStateOf(arrayOf<String>()) }
+                    if (index > 2 && !value.first().isWhitespace()) {
+                        content = value.split(Regex("\\s+"))
+                        TextField(
+                            readOnly = true,
+                            value = content[0],
+                            onValueChange = {},
+                            trailingIcon = {
+                                if (content[2].contains('d')) {
+                                    Icon(
+                                        imageVector = Icons.Rounded.Close,
+                                        contentDescription = "Down",
+                                        modifier = Modifier.background(color = Color.Red)
                                     )
+                                } else Icon(
+                                    imageVector = Icons.Rounded.Check,
+                                    contentDescription = "Up",
+                                    modifier = Modifier.background(color = Color.Green)
                                 )
+                            },
+                            enabled = false,
+                            modifier = Modifier.clickable {
+                                onRequest()
+                                VyOSConnection.getVyOSData("\"interfaces\"",
+                                    onSuccess = {
+                                        addresses = emptyArray()
+                                        if (it.data!!.findValue(content[0]).get("address") != null) {
+                                            if (it.data!!.findValue(content[0]).get("address").isArray) {
+                                                it.data!!.findValue(content[0]).get("address").forEach { address ->
+                                                    addresses += address.textValue()
+                                                }
+                                            } else {
+                                                addresses += it.data!!.findValue(content[0]).get("address").textValue()
+                                            }
+                                        }
+                                        displayDetails = true
+                                    }, onError = {
+                                        onError(it)
+                                    })
+
                             }
+                        )
+                        if(displayDetails){
+                            AlertDialog(
+                                onDismissRequest = { displayDetails = false },
+                                confirmButton = {
+                                    Button(onClick = { displayDetails = false }) {
+                                        Text("Close")
+                                    }
+                                },
+                                title = { Text(text = "Interface addresses") },
+                                text = {
+                                    Column {
+                                        if(addresses.isEmpty()){
+                                            Text("No addresses assigned")
+                                        }
+                                        for (address in addresses) {
+                                            Text(text = address)
+                                        }
+                                    }
+                                }
+                            )
                         }
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "MEMORY", fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(text = memory)
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "UPTIME", fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(text = uptime.split("\n").first())
+                }
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = "CPU", fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.height(10.dp))
+                cpu.split("\n").forEachIndexed { index, s ->
+                    if(index > 1){
+                        Text(text = s.replace(Regex("\\s+"), " "))
                     }
                 }
             }
         }
     }
 }
+
 
 @Preview
 @Composable
--- app/src/main/java/pl/lapko/vyosmanager/ui/displays/RouteTableDisplay.kt
+++ app/src/main/java/pl/lapko/vyosmanager/ui/displays/RouteTableDisplay.kt
@@ -1,11 +1,12 @@
 package pl.lapko.vyosmanager.ui.displays
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,14 +18,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import pl.lapko.vyosmanager.VyOSConnection
 
 @Composable
-fun RouteTableDisplay(onRequest : () -> Unit, onError : (Exception) -> Unit){
+fun RouteTableDisplay(onRequest : () -> Unit, onError : (Exception) -> Unit) {
     var routes by remember { mutableStateOf(listOf<String>()) }
     var displayRouteTable by remember { mutableStateOf(false) }
     LaunchedEffect(Unit) {
@@ -47,38 +46,19 @@ fun RouteTableDisplay(onRequest : () -> Unit, onError : (Exception) -> Unit){
                 fontWeight = FontWeight.Bold
             )
             Spacer(modifier = Modifier.padding(10.dp))
-            Text(
-                text = routes[0],
-                fontSize = 10.sp
-            )
-            Spacer(modifier = Modifier.padding(10.dp))
-            LazyColumn {
+            LazyColumn(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
                 itemsIndexed(routes[1].removeSuffix("\n").split("\n")) { index, value ->
-                    val tag = Regex("[^1-9]*\\s+").find(value)!!.value
-                    LazyRow {
-                        item {
-                            Text(
-                                text = tag,
-                                fontFamily = FontFamily.Monospace,
-                                modifier = Modifier.border(
-                                    width = 1.dp,
-                                    color = Color.White
-                                )
-                            )
-                        }
-                        itemsIndexed(value.removePrefix(tag).split(", ")) {index, value ->
-                            Text(
-                                text = value,
-                                fontFamily = FontFamily.Monospace,
-                                modifier = Modifier.border(
-                                    width = 1.dp,
-                                    color = Color.White
-                                )
-                            )
-                        }
-                    }
+                    val tag = Regex("[^0-9]*\\s+").find(value)!!.value
+                    Text(
+                        text = value.removePrefix(tag).split(", ").first(),
+                        modifier = Modifier.border(
+                            width = 1.dp,
+                            color = Color.DarkGray
+                        ).fillMaxWidth().padding(10.dp)
+                    )
                 }
             }
         }
     }
 }
+
